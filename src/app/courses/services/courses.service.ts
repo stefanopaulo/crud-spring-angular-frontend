d76cb094185e3ee96a05b0502f216a87ba5135f0
Stefano Souza <stefano.desouza@vmetrix.com>
2021-10-26 16:57:36 -0300

    Course List: Spinner (Loading)

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { first, tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 
@@ -17,6 +17,7 @@ export class CoursesService {
     return this.http.get<Course[]>(this.API)
       .pipe(
         first(),
+        delay(5000),
         tap(courses => console.log(courses))
       );
   }
