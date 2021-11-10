4008508a6d1e4d8d2d6d378bd1fd1fa757045e39
Stefano Souza <stefano.desouza@vmetrix.com>
2021-11-10 10:38:18 -0300

    Course List: Pipe to show icon

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -9,7 +9,7 @@ import { Course } from '../model/course';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/acourses.json';
+  private readonly API = '/assets/courses.json';
 
   constructor(private http: HttpClient) { }
 
@@ -17,7 +17,7 @@ export class CoursesService {
     return this.http.get<Course[]>(this.API)
       .pipe(
         first(),
-        delay(5000),
+        // delay(5000),
         tap(courses => console.log(courses))
       );
   }
