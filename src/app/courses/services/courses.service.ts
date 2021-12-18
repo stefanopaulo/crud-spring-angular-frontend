e182925ffc6df60d33f5497752179fcd33190056
Stefano Souza <stefano.desouza@vmetrix.com>
2021-12-18 16:36:23 -0300

    H2 Database and Connecting Angular to the Spring API

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, first, tap } from 'rxjs/operators';
+import { first, tap } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 
@@ -9,7 +9,7 @@ import { Course } from '../model/course';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private http: HttpClient) { }
 
@@ -17,7 +17,6 @@ export class CoursesService {
     return this.http.get<Course[]>(this.API)
       .pipe(
         first(),
-        // delay(5000),
         tap(courses => console.log(courses))
       );
   }
